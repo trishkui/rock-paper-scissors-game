dd0822ea36d2128d5849c19ac1e212e86e5038bb
trishkui <graceirungu907@gmail.com>
2024-09-20 16:40:31 +0300

    game

---
+++ app.js
@@ -0,0 +1,51 @@
+ const comp =document.getElementById("comp");
+const resulter=document.getElementById("resulter");
+const player =document.getElementById("player");
+
+
+function computerChoice(){
+    const choices=["rock", "paper", "scissors"];
+    const choice = Math.floor(Math.random()*choices.length);
+
+    let result="";
+}
+
+// console.log(computerChoice());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const modifyEvent=(emoji)=>{
+// document.querySelector("#player-emoji").innerHTML=emoji;
+// }
